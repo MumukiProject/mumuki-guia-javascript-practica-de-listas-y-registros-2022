a335618247172c108ba44d552cff1bef0811a32e
gustavo@mumuki.org <gustavo@mumuki.org>
2022-04-07 19:38:20 +0000

    Mumuki Export on 2022-04-07 19:38:20 +0000

--- 00015_Como mínimo/extra.js
+++ 00015_Como mínimo/extra.js
@@ -11,14 +11,3 @@ function minimo(numeros) /*<elipsis-for-student@*/ {
 //
 //  ム minimo([5, 7, 8, 9, 3]) 
 //  => 3
-
-function agregar(unaLista, unElemento) /*<elipsis-for-student@*/ {
-  unaLista.push(unElemento);
-} /*@elipsis-for-student>*/
-// Inserta unElemento al final de unaLista.
-// Este es un procedimiento que no retorna nada pero modifica a unaLista:
-//
-//  ム let cancionesFavoritas = ["La colina de la vida", "Zamba por vos"]
-//  ム agregar(cancionesFavoritas, "Seminare")
-//  ム cancionesFavoritas
-//  ["La colina de la vida", "Zamba por vos", "Seminare"]
