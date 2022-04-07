ee7963ca65353e6909dbcf92dfcca04e78bd6c1d
gustavo@mumuki.org <gustavo@mumuki.org>
2022-04-07 19:37:57 +0000

    Mumuki Export on 2022-04-07 19:37:57 +0000

--- 00014_Esto es lo máximo/extra.js
+++ 00014_Esto es lo máximo/extra.js
@@ -7,14 +7,3 @@ function maximo(numeros) /*<elipsis-for-student@*/ {
 //
 //  ム maximo([4, 8, 87, 3])
 //  87
-
-function agregar(unaLista, unElemento) /*<elipsis-for-student@*/ {
-  unaLista.push(unElemento);
-} /*@elipsis-for-student>*/
-// Inserta unElemento al final de unaLista.
-// Este es un procedimiento que no retorna nada pero modifica a unaLista:
-//
-//  ム let cancionesFavoritas = ["La colina de la vida", "Zamba por vos"]
-//  ム agregar(cancionesFavoritas, "Seminare")
-//  ム cancionesFavoritas
-//  ["La colina de la vida", "Zamba por vos", "Seminare"]
