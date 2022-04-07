6505a70ebae3e1f711b79f73e6a9b9a19b50602c
gustavo@mumuki.org <gustavo@mumuki.org>
2022-04-07 19:36:13 +0000

    Mumuki Export on 2022-04-07 19:36:13 +0000

--- 00010_Un promedio más positivo/extra.js
+++ 00010_Un promedio más positivo/extra.js
@@ -3,7 +3,7 @@
 /*...solution[9]...*/
 
 function promedio(listaDeNumeros) {
-	return sumatoria(listaDeNumeros) / longitud(listaDeNumeros);
+	return sumatoria(listaDeNumeros) / listaDeNumeros.length;
 }
 
 function sumatoria(listaDeNumeros) {
@@ -13,26 +13,3 @@ function sumatoria(listaDeNumeros) {
   }
   return sumatoria;
 }
-
-function agregar(unaLista, unElemento) /*<elipsis-for-student@*/ {
-  unaLista.push(unElemento);
-} /*@elipsis-for-student>*/
-// Inserta unElemento al final de unaLista.
-// Este es un procedimiento que no retorna nada pero modifica a unaLista:
-//
-//  ム let cancionesFavoritas = ["La colina de la vida", "Zamba por vos"]
-//  ム agregar(cancionesFavoritas, "Seminare")
-//  ム cancionesFavoritas
-//  ["La colina de la vida", "Zamba por vos", "Seminare"]
-
-function longitud(unStringOLista) /*<elipsis-for-student@*/ {
-  return unStringOLista.length;
-} /*@elipsis-for-student>*/
-// Retorna el largo de un string o una lista
-//
-// Por ejemplo:
-//
-//  ム longitud("hola")
-//  4
-//  ム longitud([5, 6, 3])
-//  3
